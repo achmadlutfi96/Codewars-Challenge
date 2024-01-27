e1542500de7fef6cf45c6a14cbb835254f3652f0
Achmad Lutfi <alhacker1996@gmail.com>
2024-01-27 20:25:58 +0700

    Create 04-Return-Negative.js

---
+++ 8kyu/04-Return-Negative.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+    // Code?
+    return num <= 0 ? num : -num;
+  }
+
+  console.log(makeNegative(42));
+  console.log(makeNegative(0));
+  console.log(makeNegative(-42));
