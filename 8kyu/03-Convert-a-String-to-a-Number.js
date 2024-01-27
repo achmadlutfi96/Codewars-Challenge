91be6b48bbd1ed5930ecf6948be5e30b6d4d8ab3
Achmad Lutfi <alhacker1996@gmail.com>
2024-01-27 20:08:44 +0700

    Create 03-Convert-a-String-to-a-Number.js

---
+++ 8kyu/03-Convert-a-String-to-a-Number.js
@@ -0,0 +1,9 @@
+const stringToNumber = function(str){
+    // put your code here
+    // return parseInt(str);
+
+    // Use Number() for more faster
+    return Number(str);
+  }
+
+  console.log(stringToNumber("-7"));
