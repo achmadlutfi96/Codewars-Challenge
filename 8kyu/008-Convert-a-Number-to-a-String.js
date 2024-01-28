49c140c5d259145f0535ea41189a9ed4556fcd1f
Achmad Lutfi <alhacker1996@gmail.com>
2024-01-28 19:24:01 +0700

    Create 008-Convert-a-Number-to-a-String.js

---
+++ 8kyu/008-Convert-a-Number-to-a-String.js
@@ -0,0 +1,9 @@
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+
+    // or
+    // return String(num);
+}
+
+console.log(numberToString(67));
