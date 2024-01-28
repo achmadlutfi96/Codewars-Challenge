842236e68dc2db23ec1c2416a95ba8dfa62b7a83
Achmad Lutfi <alhacker1996@gmail.com>
2024-01-28 19:44:07 +0700

    Create 009-Opposite-number.js

---
+++ 8kyu/009-Opposite-number.js
@@ -0,0 +1,9 @@
+function opposite(number) {
+    //your code here
+    return  number ? number * -1 : number;
+}
+
+console.log(opposite(1));
+console.log(opposite(0));
+console.log(opposite(4.25));
+console.log(opposite(-12525220.3325));
