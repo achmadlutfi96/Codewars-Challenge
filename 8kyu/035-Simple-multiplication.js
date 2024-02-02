8870b18f15788b7eb2c242b6ab72ab5335e77227
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-02 14:47:04 +0700

    Create 035-Simple-multiplication.js

---
+++ 8kyu/035-Simple-multiplication.js
@@ -0,0 +1,9 @@
+function simpleMultiplication(number) {
+    // your code........
+    return number % 2 == 0 ? number * 8 : number * 9 ; 
+}
+
+console.info(simpleMultiplication(2));
+console.info(simpleMultiplication(1));
+console.info(simpleMultiplication(8));
+console.info(simpleMultiplication(5));
