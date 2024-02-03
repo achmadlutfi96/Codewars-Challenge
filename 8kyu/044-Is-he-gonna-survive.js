92d12e7dea7e39fa6ac32fd24d912ce023714245
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-03 17:50:06 +0700

    Create 044-Is-he-gonna-survive.js

---
+++ 8kyu/044-Is-he-gonna-survive.js
@@ -0,0 +1,11 @@
+function hero(bullets, dragons){
+    //Get Coding!
+    return bullets/dragons >= 2;  
+}
+
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1));
