fed862647122327f775a58c9a2aea04193796902
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-04 20:28:40 +0700

    Update 046-DNA-to-RNA-Conversion.js

--- 8kyu/046-DNA-to-RNA-Conversion.js
+++ 8kyu/046-DNA-to-RNA-Conversion.js
@@ -1,6 +1,10 @@
 function DNAtoRNA(dna) {
     // create a function which returns an RNA sequence from the given DNA sequence
     return dna.split('').map(i => i == 'T' ?  i='U' : i ).join('');
+
+    // alternative
+    // return dna.replace(/T/g, 'U');
+    // return dna.replaceAll('T','U');
   }
 
 console.log(DNAtoRNA("TTTT"));
