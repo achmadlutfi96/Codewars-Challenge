87a942b9ab978e400910eefcb614a2d5b7390a08
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-04 20:54:27 +0700

    Create 048-You-Cant-Code-Under-Pressure-1.js

---
+++ 8kyu/048-You-Cant-Code-Under-Pressure-1.js
@@ -0,0 +1,8 @@
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    // return i+i;
+    return i*2;
+  }
+
+  console.log(doubleInteger(2));
+  console.log(doubleInteger(8));
