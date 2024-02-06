b2fad1b4914b2b361b01f26f7ae66b714010762e
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-06 14:22:32 +0700

    Create 8kyu\060-Area-or-Perimeter.js

---
+++ 8kyu/060-Area-or-Perimeter.js
@@ -0,0 +1,7 @@
+const areaOrPerimeter = function(l , w) {
+    // Return your answer
+    return l == w ? l*w : 2*(l+w);
+  };
+
+  console.log(areaOrPerimeter(3,  3));
+  console.log(areaOrPerimeter(6, 10));
