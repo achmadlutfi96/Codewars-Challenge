de901e1ac19ebdd7792bdcf19a2f16abd1098ea3
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-13 16:19:43 +0700

    Create 8kyu\077-Twice-as-old.js

---
+++ 8kyu/077-Twice-as-old.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // your code here
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+
+  }
+
+  console.log(twiceAsOld(42,21));
+  console.log(twiceAsOld(55,30));
+  console.log(twiceAsOld(36,7));
