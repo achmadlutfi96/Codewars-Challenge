8ad0a7e584208da4ef0e0261a58b3b4a6d853925
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-14 21:14:19 +0700

    Create 8kyu\085-Parse-nice-int-from-char-problem.js

---
+++ 8kyu/085-Parse-nice-int-from-char-problem.js
@@ -0,0 +1,9 @@
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :) 
+    return Number(inputString[0])
+
+    // Alternative
+    // return Number(inputString[0])
+}
+
+console.log(getAge("4 years old"));
