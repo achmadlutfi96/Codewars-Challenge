6b6ee4e737d4fc87dcd23248cba81d6ddbf3be65
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-21 11:16:10 +0700

    Create 8kyu\147-Hex-to-Decimal.js

---
+++ 8kyu/147-Hex-to-Decimal.js
@@ -0,0 +1,6 @@
+function hexToDec(hexString) {
+  //your code here
+  return parseInt(hexString, 16); // 16 for hexadecimal, 2 for biner, 10 for decimal, 8 for octal
+}
+
+console.log(hexToDec("10"));
