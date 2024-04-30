bbf07d95ba8fc88b9a8b3568c4c070b19e10aefd
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-30 18:24:14 +0700

    Create 8kyu\175-Find-the-Remainder.js

---
+++ 8kyu/175-Find-the-Remainder.js
@@ -0,0 +1,7 @@
+function remainder(n, m){
+    // Divide the larger argument by the smaller argument and return the remainder
+    return n > m ? n%m : m%n;
+    
+  }
+
+  console.log(remainder(17,5));
